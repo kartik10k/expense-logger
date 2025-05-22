2da792367aaa185ceb89a977281ec5914fb8aa5b
Kartik <kartikkhandelwal95@gmail.com>
2025-05-22 18:34:10 +0530

    Fix PWA paths and add base tag

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = 'expense-logger-v1';
 const ASSETS_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/app.js',
-    '/manifest.json',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png'
+    './',
+    './index.html',
+    './styles.css',
+    './app.js',
+    './manifest.json',
+    './icons/icon-192x192.png',
+    './icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', (event) => {
